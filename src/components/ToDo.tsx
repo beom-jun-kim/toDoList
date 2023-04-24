fa86d70002d531c30f3d3a91f1a4269563d44fab
kimbeomjun <gimb52043@gmail.com>
2023-04-24 17:33:19 +0900

    카테고리 버튼 생성

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,12 +1,31 @@
-import { IToDo } from "../components/atoms";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "../components/atoms";
 
-function ToDo({ text }: IToDo) {
+function ToDo({ text, category, id }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+  };
   return (
     <li>
       <span>{text}</span>
-      <button>할일</button>
-      <button>하고있는일</button>
-      <button>한일</button>
+      {category !== "할일" && (
+        <button name="할일" onClick={onClick}>
+          할일
+        </button>
+      )}
+      {category !== "하고있는일" && (
+        <button name="하고있는일" onClick={onClick}>
+          하고있는일
+        </button>
+      )}
+      {category !== "한일" && (
+        <button name="한일" onClick={onClick}>
+          한일
+        </button>
+      )}
     </li>
   );
 }
